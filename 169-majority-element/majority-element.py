2ad8b68065b7affb568fa9a9975bc5abdcb7760c
Laaveshwaran P <laavesh1@gmail.com>
2024-08-06 12:54:18 +0530

    Time: 177 ms (28.92%) | Memory: 18 MB (65.68%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         d = {}
+        n = len(nums)
         for i in nums:
             try:
                 d[i] += 1
+                if d[i] > n:
+                    return i
             except:
                 d[i] = 1
+            
 
         maxi = max(d, key=lambda i:d[i])
         # maxi = max(d)
