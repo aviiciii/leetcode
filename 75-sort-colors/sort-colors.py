e3552e6c4ef72c1c1f49c2dbaef81b73b7cf99c3
Laaveshwaran P <laavesh1@gmail.com>
2024-08-06 18:37:01 +0530

    Time: 47 ms (6.10%) | Memory: 16.7 MB (16.97%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,4 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        l = 0
+        r = len(nums) -1
+        i = 0
+        while i < len(nums) and l<r:
+            if nums[i] == 0:
+                if i == l:
+                    i +=1
+                    l +=1
+                    continue
+                nums[l], nums[i] = nums[i], nums[l]
+                l += 1
+            elif nums[i] == 2:
+                if i >= r:
+                    i=len(nums)
+                    continue
+                nums[r], nums[i] = nums[i], nums[r]
+                r -= 1
+            else:
+                i+= 1
+            print(nums, i, l, r)
+        print(nums)
